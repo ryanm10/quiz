fa2313ea5348760e10fb0e4b2438eb1a87589878
Ryan <ryan.milton@patelhs.org>
2020-10-16 22:09:01 +0000

    Changed up the questions a bit, made it more clear

--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ import java.util.Scanner; //Meant to be for the scanner
 class Main { //Starting off
   public static void main(String[] args) {
   Scanner reader = new Scanner(System.in);//My scanner
-    System.out.println("Welcome to the quiz!");//Introducing and welcoming the user who is taking the quiz
+    System.out.println("Welcome to the quiz! In this short little quiz, you will be answering a question about what class period I have Ms. Gaber! :)");//Introducing and welcoming the user who is taking the quiz
     
     boolean correct = false;
   
-    while (correct == false) {
+    while (correct == false) { //While
 
     
-      System.out.println("What the class period?");//Asking what the class period I'm in is
+      System.out.println("What class period do I have Ms. Gaber?");//Asking what the class period I'm in is
       System.out.println("(A) 1st Period");//Option choice for the first class
       System.out.println("(B) 3rd Period");//Option choice for the second class
       
