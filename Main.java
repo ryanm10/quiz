e2cde59df5595d011c38e75aa2c15c7f12bbe03f
Ryan <ryan.milton@patelhs.org>
2020-10-16 18:46:44 +0000

    Added a while loop to my code. :)

--- Main.java
+++ Main.java
@@ -4,23 +4,28 @@ class Main { //Starting off
   Scanner reader = new Scanner(System.in);//My scanner
     System.out.println("Welcome to the quiz!");//Introducing and welcoming the user who is taking the quiz
     
-    boolean iscorrect = false;
-    while iscorrect = false;
-    System.out.println("What the class period?");//Asking what the class period I'm in is
-    System.out.println("(A) 1st Period");//Option choice for the first class
-    System.out.println("(B) 3rd Period");//Option choice for the second class
+    boolean correct = false;
+  
+    while (correct == false) {
 
-    String response = reader.nextLine();//Takes/accepts/reads user input
-    if (response.equals("B"))//If the user sends in B
-    {
-      System.out.println("You are correct!");//Printing correct if they Send B
-    }
-    else //If they DO NOT send B
-    {
-      System.out.println("Wrong! Sorry!");//Letting them know that B was not the answer to the question, they may retry the quiz
-    }
+    
+      System.out.println("What the class period?");//Asking what the class period I'm in is
+      System.out.println("(A) 1st Period");//Option choice for the first class
+      System.out.println("(B) 3rd Period");//Option choice for the second class
+      
+      String response = reader.nextLine();//Takes/accepts/reads user input
+      
+      if (response.equals("B"))//If the user sends in B
+      {
+        System.out.println("You are correct!");//Printing correct if they Send B
+        correct = true;
+      }
+      else //If they DO NOT send B
+      {
+        System.out.println("Wrong! Sorry!");//Letting them know that B was not the answer to the question, they may retry the quiz
+      }
   
-  
+    }
   }
 }
 //
